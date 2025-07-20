b59f908b4530ce9f8af9dbe9d572ea818ed5ab59
Asma Saud Omar <asmasauod3@gmail.com>
2025-07-20 21:08:48 +0300

    Add new Model fo rServiceDevice Table

---
+++ WepApp2/Models/ServiceDevice.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WepApp2.Models
+{
+    public class ServiceDevice
+    {
+        [Key]
+        public int ServiceDeviceID { get; set; }
+
+        [Required]
+        public int ServiceID { get; set; }
+
+        [Required]
+        public int DeviceID { get; set; }
+
+        // علاقات الربط مع الجداول الأخرى
+        [ForeignKey("ServiceID")]
+        public Service Service { get; set; }
+
+        [ForeignKey("DeviceID")]
+        public Device Device { get; set; }
+    }
+}
